2e0454ac57c4b1cbba6014f22feec2a07fd1e63a
Chris Dzombak <chris@chrisdzombak.net>
2017-11-27 22:04:10 -0500

    Initial commit

---
+++ lamp-on.js
@@ -0,0 +1,7 @@
+const { Client } = require('tplink-smarthome-api');
+
+const client = new Client();
+const plug = client.getDevice({host: '192.168.1.54'}).then((device) => {
+  device.getSysInfo().then(console.log);
+  device.setPowerState(true);
+});
